8c85d071eee7a0b6c66351f2574439bafa10d23f
Nicorg4 <nicolasgota@hotmail.com>
2024-09-08 02:21:13 -0300

    Added cors

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const studentRoutes = require('./routes/studentRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
 const subjectRoutes = require('./routes/subjectRoutes');
 const autenthicationRoutes = require('./routes/authenticationRoutes'); 
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }));
 
 app.use(express.json());
 app.use('/students', studentRoutes);
 app.use('/teachers', teacherRoutes);
 app.use('/authentication', autenthicationRoutes);
-    app.use('/subject', subjectRoutes);
+app.use('/subject', subjectRoutes);
 
 module.exports = app;
