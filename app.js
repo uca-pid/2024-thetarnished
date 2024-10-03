dae999c1ea58fb54adeab8f5dbacd22902d5ac64
Nicorg4 <nicolasgota@hotmail.com>
2024-10-03 09:14:22 -0300

    route redirection fix

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const studentRoutes = require('./routes/studentRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
 const subjectRoutes = require('./routes/subjectRoutes');
@@ -28,7 +29,11 @@ app.use('/authentication', autenthicationRoutes);
 app.use('/subject', subjectRoutes);
 app.use('/schedule', scheduleRoutes);
 app.use('/reservation', reservationRoutes);
-app.use('/classes', monthlyScheduleRoutes)
+app.use('/classes', monthlyScheduleRoutes);
 app.use('/admins', adminRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
