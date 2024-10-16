00745b1e10864708504514f8f1159ed44af98c00
Baltasar Turanza <102920870+waterzone1@users.noreply.github.com>
2024-10-16 02:43:46 -0300

    added get exams functionality

--- controllers/examController.js
+++ controllers/examController.js
@@ -174,14 +174,71 @@ const getExamsByStudentId = async (req, res) => {
     }
 };
 
-getExamsById = async (req, res) => {
+const getExamsById = async (req, res) => {
     try {
         const { exam_id } = req.params;
-        const exam = await Exam.findByPk(exam_id);
-        if (!exam) {
-            return res.status(404).json({ message: 'Exam not found' });
+
+        const query = `
+            SELECT e.exam_id, e.exam_name, t.firstname AS teacher_firstname, t.lastname AS teacher_lastname, 
+                   s.subjectname, q.question_id, q.question_text, c.choice_id, c.choice_text, c.is_correct
+            FROM exams e
+            JOIN teachers t ON e.teacher_id = t.teacherid
+            JOIN subjects s ON e.subject_id = s.subjectid
+            JOIN questions q ON e.exam_id = q.exam_id
+            JOIN choices c ON q.question_id = c.question_id
+            WHERE e.exam_id = :exam_id
+            ORDER BY q.question_id, c.choice_id;
+        `;
+
+        const exams = await sequelize.query(query, {
+            replacements: { exam_id }, 
+            type: sequelize.QueryTypes.SELECT
+        });
+
+        if (!exams.length) {
+            return res.status(404).json({ message: 'No exam found with this ID' });
         }
-        return res.status(200).json(exam);
+
+        const formattedExams = {};
+        exams.forEach(exam => {
+            if (!formattedExams[exam.exam_id]) {
+                formattedExams[exam.exam_id] = {
+                    exam_id: exam.exam_id,
+                    exam_name: exam.exam_name,
+                    teacher: {
+                        firstname: exam.teacher_firstname,
+                        lastname: exam.teacher_lastname,
+                    },
+                    subject: exam.subjectname,
+                    questions: {},
+                };
+            }
+
+            if (!formattedExams[exam.exam_id].questions[exam.question_id]) {
+                formattedExams[exam.exam_id].questions[exam.question_id] = {
+                    question_id: exam.question_id,
+                    question_text: exam.question_text,
+                    choices: [],
+                };
+            }
+
+            formattedExams[exam.exam_id].questions[exam.question_id].choices.push({
+                choice_id: exam.choice_id,
+                choice_text: exam.choice_text,
+                is_correct: exam.is_correct,
+            });
+        });
+
+
+        const result = Object.values(formattedExams).map(exam => {
+            return {
+                ...exam,
+                questions: Object.values(exam.questions)
+            };
+        });
+
+        return res.status(200).json(result);
+
     } catch (err) {
         console.error(err);
         return res.status(500).json({
@@ -197,5 +254,6 @@ module.exports = {
     deleteExam,
     getExamsByTeacherId,
     getExamsByStudentId,
+    getExamsById,
   };
 
--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -4,6 +4,7 @@ const {
     deleteExam,
     getExamsByTeacherId,
     getExamsByStudentId,
+    getExamsById,
 } = require('../controllers/examController');
 
 const router = express.Router();
@@ -12,6 +13,7 @@ router.post('/create-exam', createExamWithQuestions);
 router.delete('/delete-exam', deleteExam);
 router.get('/get-teacher-exams-by/:teacher_id', getExamsByTeacherId);
 router.get('/get-student-exams-by/:student_id', getExamsByStudentId);
+router.get('/get-exams-by/:exam_id', getExamsById)
 
 
 module.exports = router;
