c40190f4cebe39bce6350a4b54ce4381834192c6
Nicorg4 <81263174+Nicorg4@users.noreply.github.com>
2024-10-17 20:40:59 -0300

    route permissions

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -15,7 +15,7 @@ const authorizeRoles = require('../middleware/authMiddleware');
 
 router.get('/all-dictating/:subjectid', authorizeRoles('STUDENT'), getAllTeachersDictatingASubjectById);
 router.get('/all-teachers', authorizeRoles('ADMIN'), getAllTeachers);
-router.get('/:id', authorizeRoles('ADMIN'), getTeacherById);
+router.get('/:id', authorizeRoles('ADMIN', 'STUDENT', 'TEACHER'), getTeacherById);
 router.put('/update/:id', authorizeRoles('TEACHER'), updateTeacher);
 router.delete('/delete/:id', authorizeRoles('TEACHER', 'ADMIN'), deleteTeacher);
 router.post('/assign-subject/:teacherid', authorizeRoles('ADMIN', 'TEACHER'), assignSubjectToTeacher);
